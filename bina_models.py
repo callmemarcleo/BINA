4a1cf1ca28a03b5d4a022e98684eef006ff85919
callmemarcleo <marc.hoesli@students.bfh.ch>
2025-04-05 16:59:05 +0200

    [delete dataclass]: neighbourhood and reviews no longer exist
    Name of the table "Listing" is the same, it is just the detailed version. The other two tables "neigbourhood" and "reviews" were deleted because the data is also in the detailed version of "Listing"

--- bina_models.py
+++ bina_models.py
@@ -8,29 +8,24 @@ class Listing:
     name: str
     host_id: int
     host_name: str
-    neighbourhood_group: str
-    neighbourhood: str
+    neighbourhood_group_cleansed: str
+    neighbourhood_cleansed: str
     latitude: float
     longitude: float
+    property_type: str
     room_type: str
+    accommodates: int
+    bathrooms: Optional[float]
+    bathrooms_text: Optional[str]
+    bedrooms: Optional[int]
+    beds: Optional[int]
     price: Optional[float]
     minimum_nights: int
     number_of_reviews: int
     last_review: Optional[str]
+    review_scores_rating: Optional[float]
     reviews_per_month: Optional[float]
     calculated_host_listings_count: int
     availability_365: int
     number_of_reviews_ltm: str
     license: Optional[str]
-
-
-@dataclass
-class Neighbourhood:
-    neighbourhood_group: str
-    neighbourhood: str
-
-
-@dataclass
-class Review:
-    listing_id: int
-    date: str
